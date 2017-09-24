e99e49a16221aa5aaa3f8955909a3317c2a3521a
Sunny Ouyang <shenny@sunnys-mbp>
2017-09-24 13:28:04 -0700

    Initial Commit

---
+++ ProductHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProductHunt
+//
+//  Created by Sunny Ouyang on 9/24/17.
+//  Copyright © 2017 Sunny Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
